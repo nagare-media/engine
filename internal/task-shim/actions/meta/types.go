e57e38dc2679deb042be285984f757e344ac5690
Matthias Neugebauer <mtneug@mailbox.org>
2023-11-13 11:29:49 +0100

    Add meta actions

---
+++ internal/task-shim/actions/meta/types.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2022-2023 The nagare media authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package meta
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/nagare-media/engine/internal/task-shim/actions"
+)
+
+// Action description
+const (
+	Name = actions.BuildInActionPrefix + "meta"
+)
+
+// ConfigType of the meta action.
+type ConfigType string
+
+const (
+	ConfigTypeStartTask   ConfigType = "start-task"
+	ConfigTypeRestartTask ConfigType = "restart-task"
+	ConfigTypeStopTask    ConfigType = "stop-task"
+)
+
+func (ct *ConfigType) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	switch c := ConfigType(s); c {
+	case ConfigTypeStartTask,
+		ConfigTypeRestartTask,
+		ConfigTypeStopTask:
+		*ct = c
+		return nil
+	}
+
+	return errors.New("meta: unknown config type")
+}
